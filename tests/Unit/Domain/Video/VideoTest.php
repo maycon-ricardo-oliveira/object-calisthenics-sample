c5a7582b548188fe722352dc0a5f5eae5e70d611
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-05-05 22:10:40 -0300

    feat: change name function

--- tests/Unit/Domain/Video/VideoTest.php
+++ tests/Unit/Domain/Video/VideoTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class VideoTest extends TestCase
 {
-    public function testChangeVisibilityMustWork()
+    public function testMakingAVideoPublicMustWork()
     {
         $video = new Video();
         $video->checkIfVisibilityIsValidAndUpdateIt(Video::PUBLIC);
